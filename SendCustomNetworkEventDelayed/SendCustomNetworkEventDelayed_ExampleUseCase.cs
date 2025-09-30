410c4907f7f038b2a92f56709c313135f7361906
Occala <wawaw36912@gmail.com>
2025-09-30 12:48:07 -0400

    Add files for SendCustomNetworkEventDelayed

---
+++ SendCustomNetworkEventDelayed/SendCustomNetworkEventDelayed_ExampleUseCase.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) 2025 Occala
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.SDK3.UdonNetworkCalling;
+
+namespace Occala.RandomVRCScripts
+{
+    [AddComponentMenu("")]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class SendCustomNetworkEventDelayed_ExampleUseCase : UdonSharpBehaviour
+    {
+        private void Start()
+        {
+            SendCustomEventDelayedSeconds(nameof(_RepeatedSend), Random.Range(1f, 5f));
+        }
+
+        public void _RepeatedSend()
+        {
+            float delay = Random.Range(1f, 2.5f);
+            SendCustomNetworkEventDelayed_Manager.NetworkEventDelayedSeconds(
+                this,
+                VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(ExampleNetworkEvent),
+                delay,
+                Random.Range(int.MinValue, int.MaxValue), Random.Range(float.MinValue, float.MaxValue), "hello!"
+                );
+
+            SendCustomEventDelayedSeconds(nameof(_RepeatedSend), Random.Range(1f, 5f));
+        }
+
+        [NetworkCallable]
+        public void ExampleNetworkEvent(int integer, float single, string stringValue)
+        {
+            Debug.Log($"[{nameof(ExampleNetworkEvent)}] Invoked - Parameters: {integer} | {single} | {stringValue}");
+        }
+
+    }
+
+}
