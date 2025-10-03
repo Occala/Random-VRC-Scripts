788d751c9a4624fdc47d8bf91f06439bbc31477d
Occala <wawaw36912@gmail.com>
2025-10-03 08:53:36 -0400

    Update SendCustomNetworkEventDelayed_ExampleUseCase.cs

--- SendCustomNetworkEventDelayed/SendCustomNetworkEventDelayed_ExampleUseCase.cs
+++ SendCustomNetworkEventDelayed/SendCustomNetworkEventDelayed_ExampleUseCase.cs
@@ -13,7 +13,8 @@ namespace Occala.RandomVRCScripts
     public class SendCustomNetworkEventDelayed_ExampleUseCase : UdonSharpBehaviour
     {
         private SendCustomNetworkEventDelayed_Manager _manager;
-        
+        private bool flipFlop;
+
         private void Start()
         {
             // Cache the manager instance once for performance
@@ -26,11 +27,26 @@ namespace Occala.RandomVRCScripts
         {
             float delay = Random.Range(1f, 2.5f);
 
-            _manager.NetworkEventDelayedSeconds(
-                this, delay,
-                VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(ExampleNetworkEvent),
-                Random.Range(int.MinValue, int.MaxValue), Random.Range(float.MinValue, float.MaxValue), "hello!"
-                );
+            if (flipFlop)
+            {
+                // Note that there is a call you can make without caching the manager, NetworkEventDelayedSeconds_Expensive()
+                _manager.NetworkEventDelayedSeconds(
+                    this, delay,
+                    VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(ExampleNetworkEvent),
+                    Random.Range(int.MinValue, int.MaxValue), Random.Range(float.MinValue, float.MaxValue), "hello!"
+                    );
+            }
+            else
+            {
+                // Note that there is a call you can make without caching the manager, NetworkEventDelayedFrames_Expensive()
+                _manager.NetworkEventDelayedFrames(
+                    this, 1,
+                    VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(ExampleNetworkEvent),
+                    Random.Range(int.MinValue, int.MaxValue), Random.Range(float.MinValue, float.MaxValue), "hello!"
+                    );
+            }
+
+            flipFlop = !flipFlop;
 
             SendCustomEventDelayedSeconds(nameof(_RepeatedSend), Random.Range(1f, 5f));
         }
@@ -38,7 +54,7 @@ namespace Occala.RandomVRCScripts
         [NetworkCallable]
         public void ExampleNetworkEvent(int integer, float single, string stringValue)
         {
-            Debug.Log($"[{nameof(ExampleNetworkEvent)}] Invoked - Parameters: {integer} | {single} | {stringValue}");
+            Debug.Log($"[{nameof(ExampleNetworkEvent)}][F:{Time.frameCount}] Invoked - Parameters: {integer} | {single} | {stringValue}");
         }
     }
 }
